562daa18eb4976e10ee1f0541d93ab4ae2e37163
Mallhar03 <ptavinbawgikar1@gmail.com>
2025-08-19 01:01:27 +0530

    fixed minor bugs

--- ddos_generator.cpp
+++ ddos_generator.cpp
@@ -7,6 +7,9 @@
 #include <iomanip>
 #include <fstream>
 
+std::string getTimestamp();
+std::string getRandomAttackType(std::mt19937 &gen);
+
 std::string getRandomIP (std::mt19937 &gen){
     std::uniform_int_distribution<> dist(0,255); //
     return  std::to_string(dist(gen)) + "." +
@@ -19,14 +22,14 @@ std::string getTimestamps(){
     auto t = std::time(nullptr);
     auto tm = *std::localtime(&t);
     std::ostringstream oss;
-    oss << std::put_time(&tm , %Y-%m-%d %H:%M:%:%S );
+    oss << std::put_time(&tm , "%Y-%m-%d %H:%M:%:%S" );
     return oss.str(); // pulls the string from the string buffer which is stored in the oss (scratchpad)
 }
 
 std::string getRandomAttacktType(std::mt19937 &gen){
-    std::vector<std::string> attackTypes = { "UDP Flood","SYN Flood","ICMP Flood","HTTP Flood"}
+    std::vector<std::string> attackTypes = { "UDP Flood","SYN Flood","ICMP Flood","HTTP Flood"};
     std::uniform_int_distribution<> dist(0, attackTypes.size()-1);
-    return attackTypes[dist(gen)] //index of the random attack which will be returned
+    return attackTypes[dist(gen)]; //index of the random attack which will be returned
 }
 
 std::string generateLogEntry(std::mt19937 &gen) {
@@ -40,18 +43,18 @@ std::string generateLogEntry(std::mt19937 &gen) {
 int main(){
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::ofstream logdile("ddos_log.txt");
+    std::ofstream logfile("ddos_log.txt");
 
     if(!logfile.is_open()){
         std::cerr<<"Error opening log file!"<<std::endl;
         return 1;
     }
 
-    for( int i =0; int < 50; i++ ){
+    for( int i = 0; i < 50; i++ ){
         logfile << generateLogEntry(gen) << std::endl; 
     }
 
     logfile.close();
-    std::cout << "Fake DDoS logs generated in ddos_log.txt ✅" <<std""endl;
+    std::cout << "Fake DDoS logs generated in ddos_log.txt ✅" <<std::endl;
     return 0;
 }
